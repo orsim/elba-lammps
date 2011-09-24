bd31d22ec08f656e269495f062e508d3d0b17e0b
orsimario <orsimario@gmail.com>
2011-09-24 13:25:58 +0000

    Added angle_dipole style

--- lmp/src/angle_dipole.cpp
+++ lmp/src/angle_dipole.cpp
@@ -17,7 +17,7 @@
 
 #include "math.h"
 #include "stdlib.h"
-#include "bond_dipole.h"
+#include "angle_dipole.h"
 #include "atom.h"
 #include "neighbor.h"
 #include "domain.h"
@@ -30,11 +30,11 @@ using namespace LAMMPS_NS;
 
 /* ---------------------------------------------------------------------- */
 
-BondDipole::BondDipole(LAMMPS *lmp) : Bond(lmp) {}
+AngleDipole::AngleDipole(LAMMPS *lmp) : Angle(lmp) {}
 
 /* ---------------------------------------------------------------------- */
 
-BondDipole::~BondDipole()
+AngleDipole::~AngleDipole()
 {
   if (allocated) {
     memory->destroy(setflag);
@@ -45,7 +45,7 @@ BondDipole::~BondDipole()
 
 /* ---------------------------------------------------------------------- */
 
-void BondDipole::compute(int eflag, int vflag)
+void AngleDipole::compute(int eflag, int vflag)
 /* ----------------------------------------------------------------------
    This function implements an intramolecular pair interaction between a
    dipolar atom 'iDip' and a reference atom 'iRef'.
@@ -53,30 +53,32 @@ void BondDipole::compute(int eflag, int vflag)
    the direction defined by the vector 'x[iRef] - x[iDip]'.
 ------------------------------------------------------------------------- */
 {
-  int iRef,iDip,n,type;
-  double delx,dely,delz,ebond,tbond;
+  int iRef,iDip,iDummy,n,type;
+  double delx,dely,delz;
+  double eangle,tangle,f1[3],f3[3];
   double r,dr,cosGamma,deltaGamma,kdg,rmu;
 
-  ebond = 0.0;
+  eangle = 0.0;
   if (eflag || vflag) ev_setup(eflag,vflag);
   else evflag = 0;
 
-  double **x = atom->x;
-  double **mu = atom->mu;
+  double **x = atom->x; 
+  double **mu = atom->mu; // point dipole
   double **torque = atom->torque;
-  int **bondlist = neighbor->bondlist;
-  int nbondlist = neighbor->nbondlist;
+  int **anglelist = neighbor->anglelist;
+  int nanglelist = neighbor->nanglelist;
   int nlocal = atom->nlocal;
   int newton_bond = force->newton_bond;
 
-  for (n = 0; n < nbondlist; n++) {
-    iDip = bondlist[n][0];
-    iRef = bondlist[n][1];
-    type = bondlist[n][2];
+  for (n = 0; n < nanglelist; n++) {
+    iDip = anglelist[n][0];
+    iRef = anglelist[n][1];
+    iDummy = anglelist[n][2];
+    type = anglelist[n][3];
 
-    delx = x[iRef][0] - x[iDip][0]; // x21
-    dely = x[iRef][1] - x[iDip][1]; // y21
-    delz = x[iRef][2] - x[iDip][2]; // z21
+    delx = x[iRef][0] - x[iDip][0];
+    dely = x[iRef][1] - x[iDip][1];
+    delz = x[iRef][2] - x[iDip][2];
     domain->minimum_image(delx,dely,delz);
 
     r = sqrt(delx*delx + dely*dely + delz*delz);
@@ -86,30 +88,37 @@ void BondDipole::compute(int eflag, int vflag)
     deltaGamma = cosGamma - cos(gamma0[type]);
     kdg = k[type] * deltaGamma;
 
-    if (eflag) ebond = kdg * deltaGamma; // energy  
+    if (eflag) eangle = kdg * deltaGamma; // energy  
       
-    tbond = 2.0 * kdg / rmu; 
+    tangle = 2.0 * kdg / rmu; 
       
-    torque[iDip][0] += tbond * (dely*mu[iDip][2] - delz*mu[iDip][1]);
-    torque[iDip][1] += tbond * (delz*mu[iDip][0] - delx*mu[iDip][2]);
-    torque[iDip][2] += tbond * (delx*mu[iDip][1] - dely*mu[iDip][0]);
+    torque[iDip][0] += tangle * (dely*mu[iDip][2] - delz*mu[iDip][1]);
+    torque[iDip][1] += tangle * (delz*mu[iDip][0] - delx*mu[iDip][2]);
+    torque[iDip][2] += tangle * (delx*mu[iDip][1] - dely*mu[iDip][0]);
     
-    if (evflag) 
-      ev_tally(iDip,iRef,nlocal,newton_bond,ebond,0.0,delx,dely,delz);
+    f1[0] = 0.0;
+    f1[1] = 0.0;
+    f1[2] = 0.0;
+    f3[0] = 0.0;
+    f3[1] = 0.0;
+    f3[2] = 0.0;
+    
+    if (evflag) ev_tally(iRef,iDip,iDummy,nlocal,newton_bond,eangle,f1,f3,
+ 			 delx,dely,delz,delx,dely,delz);
   }
 }
 
 /* ---------------------------------------------------------------------- */
 
-void BondDipole::allocate()
+void AngleDipole::allocate()
 {
   allocated = 1;
-  int n = atom->nbondtypes;
+  int n = atom->nangletypes;
 
-  memory->create(k,n+1,"bond:k");
-  memory->create(gamma0,n+1,"bond:gamma0");
+  memory->create(k,n+1,"angle:k");
+  memory->create(gamma0,n+1,"angle:gamma0");
 
-  memory->create(setflag,n+1,"bond:setflag");
+  memory->create(setflag,n+1,"angle:setflag");
   for (int i = 1; i <= n; i++) setflag[i] = 0;
 }
 
@@ -117,71 +126,70 @@ void BondDipole::allocate()
    set coeffs for one or more types
 ------------------------------------------------------------------------- */
 
-void BondDipole::coeff(int narg, char **arg)
+void AngleDipole::coeff(int narg, char **arg)
 {
-  if (narg != 3) error->all("Incorrect args for bond coefficients");
+  if (narg != 3) error->all("Incorrect args for angle coefficients");
   if (!allocated) allocate();
 
   int ilo,ihi;
-  force->bounds(arg[0],atom->nbondtypes,ilo,ihi);
+  force->bounds(arg[0],atom->nangletypes,ilo,ihi);
 
   double k_one = force->numeric(arg[1]);
   double gamma0_one = force->numeric(arg[2]);
 
+  // convert gamma0 from degrees to radians
+
   int count = 0;
-  const double PI = 3.14159265358979323846; 
   for (int i = ilo; i <= ihi; i++) {
     k[i] = k_one;
-    gamma0[i] = gamma0_one * PI / 180.0; // convert from degrees to radians
+    gamma0[i] = gamma0_one/180.0 * PI;
     setflag[i] = 1;
     count++;
   }
 
-  if (count == 0) error->all("Incorrect args for bond coefficients");
+  if (count == 0) error->all("Incorrect args for angle coefficients");
+}
+
+/* ---------------------------------------------------------------------- */
+
+double AngleDipole::equilibrium_angle(int i)
+{
+  return gamma0[i];
 }
 
 /* ----------------------------------------------------------------------
-   return an equilibrium bond length (used by SHAKE - not relevant here) 
+   proc 0 writes out coeffs to restart file
 ------------------------------------------------------------------------- */
 
-double BondDipole::equilibrium_distance(int i)
+void AngleDipole::write_restart(FILE *fp)
 {
-  return 0.0;
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 writes out coeffs to restart file 
-------------------------------------------------------------------------- */
-
-void BondDipole::write_restart(FILE *fp)
-{
-  fwrite(&k[1],sizeof(double),atom->nbondtypes,fp);
-  fwrite(&gamma0[1],sizeof(double),atom->nbondtypes,fp);
+  fwrite(&k[1],sizeof(double),atom->nangletypes,fp);
+  fwrite(&gamma0[1],sizeof(double),atom->nangletypes,fp);
 }
 
 /* ----------------------------------------------------------------------
    proc 0 reads coeffs from restart file, bcasts them 
 ------------------------------------------------------------------------- */
 
-void BondDipole::read_restart(FILE *fp)
+void AngleDipole::read_restart(FILE *fp)
 {
   allocate();
 
   if (comm->me == 0) {
-    fread(&k[1],sizeof(double),atom->nbondtypes,fp);
-    fread(&gamma0[1],sizeof(double),atom->nbondtypes,fp);
+    fread(&k[1],sizeof(double),atom->nangletypes,fp);
+    fread(&gamma0[1],sizeof(double),atom->nangletypes,fp);
   }
-  MPI_Bcast(&k[1],atom->nbondtypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&gamma0[1],atom->nbondtypes,MPI_DOUBLE,0,world);
+  MPI_Bcast(&k[1],atom->nangletypes,MPI_DOUBLE,0,world);
+  MPI_Bcast(&gamma0[1],atom->nangletypes,MPI_DOUBLE,0,world);
 
-  for (int i = 1; i <= atom->nbondtypes; i++) setflag[i] = 1;
+  for (int i = 1; i <= atom->nangletypes; i++) setflag[i] = 1;
 }
 
 /* ----------------------------------------------------------------------
    used by ComputeBondLocal - not relevant here
 ------------------------------------------------------------------------- */
 
-double BondDipole::single(int type, double rsq, int i, int j)
+double AngleDipole::single(int type, int iRef, int iDip, int iDummy)
 {
   return 0.0;
 }
---
+++ lmp/src/angle_dipole.h
@@ -0,0 +1,48 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef ANGLE_CLASS
+
+AngleStyle(dipole,AngleDipole)
+
+#else
+
+#ifndef LMP_ANGLE_DIPOLE_H
+#define LMP_ANGLE_DIPOLE_H
+
+#include "stdio.h"
+#include "angle.h"
+
+namespace LAMMPS_NS {
+
+class AngleDipole : public Angle {
+ public:
+  AngleDipole(class LAMMPS *);
+  ~AngleDipole();
+  void compute(int, int);
+  void coeff(int, char **);
+  double equilibrium_angle(int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  double single(int, int, int, int);
+
+ private:
+  double *k,*gamma0;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif
