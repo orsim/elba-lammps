7c7e061487e718270f59656a5bca34ef375d3de0
orsimario <orsimario@gmail.com>
2011-10-05 16:20:19 +0000

    Debugged *Psi tools, small changes to other tools

--- tools/press2surfTens.py
+++ tools/press2surfTens.py
@@ -1,22 +1,12 @@
-# Copyright (C) 2011 Mario Orsi
+#!/usr/bin/env python
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#----------------------------------------------------------------------
+# Script:  press2surfTens.py
 # Purpose: this script calculates the surface tension using the
 #          standard formula: 0.5*Lz*(Pzz-0.5(Pxx+Pyy))
 # Assumptions: Pxx, Pyy and Pzz are provided in units of atm (LAMMPS
 #              convention for 'units real' style) and Lz in Angstrom
 # Usage: python press2surfTens.py Lz Pxx Pyy Pzz 
+# Author:  Mario Orsi (orsimario at gmail.com)
 
 import sys, string, linecache
 from math import sqrt
