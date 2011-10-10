8a39821ab9b45e5cca5ce9d1da0d6351b6d28048
orsimario@gmail.com <orsimario@gmail.com@6a28ad32-b92a-d78a-e47c-38e9be169395>
2011-10-10 15:14:20 +0000

    Added calculation of avgs over the two monolayers. Plus some tidying.

--- trunk/tools/lpp2curv.py
+++ trunk/tools/lpp2curv.py
@@ -9,7 +9,6 @@
 # Notes: - Coordinates (first col of input file) in Angstrom
 #        - Lateral pressure Pt-Pn (second col of input file) in Atm
 #        - Coordinates' origin = bilayer center
-#        - Not tested on odd numbers of slabs
 # Example: lpp2curv.py lpp.dat 
 # References: - Orsi & Essex, PLoS ONE, submitted
 #             - Orsi et al, J Phys: Condens Matter 22, 155106 (2010)
@@ -46,46 +45,50 @@ for line in lines:
 zBox = coord[nSlabs-1] - coord[0]
 delta = zBox / ( nSlabs - 1 )
 
-nSlabsHalf = nSlabs / 2;
-
 tau1m_z = [] # fist int mom profile
 tau2m_z = [] # second int mom profile
 outFile1 = open('tau1m.dat', 'w')
 outFile2 = open('tau2m.dat', 'w')
 
 # calc 1st integral moment of lpp across 'negative' monolayer:
-tau1m = 0 # initialize 1st mom integral sum
-tau2m = 0 # initialize 2nd mom integral sum
-for i in range( nSlabsHalf-1, -1, -1 ): # i = nSlabsHalf-1, ..., 0
-  tau1m += (-coord[i]) * lp[i] * delta
-  tau2m += coord[i]**2 * lp[i] * delta
-  tau1m_z.insert(0,tau1m) # insert at the front
-  tau2m_z.insert(0,tau2m) # insert at the front
-tau1m = tau1m * atmA2__in__J_nm
-tau2m = tau2m * atmA3__in__J
+tau1mNeg = 0 # initialize 1st mom integral sum
+tau2mNeg = 0 # initialize 2nd mom integral sum
+for i in range( nSlabs/2 - 1, -1, -1 ): # i = nSlabs/2 - 1, ..., 0
+  tau1mNeg += (-coord[i]) * lp[i] * delta
+  tau2mNeg += coord[i]**2 * lp[i] * delta
+  tau1m_z.insert(0,tau1mNeg) # insert at the front
+  tau2m_z.insert(0,tau2mNeg) # insert at the front
+tau1mNeg = tau1mNeg * atmA2__in__J_nm
+tau2mNeg = tau2mNeg * atmA3__in__J
 print ( '1st int mom for \'-\' monolayer: tau1m = %e J/nm = %f kBT/nm'
-        % ( tau1m, tau1m * J__in__kBTroom ) )
+        % ( tau1mNeg, tau1mNeg * J__in__kBTroom ) )
 print ( '2nd int mom for \'-\' monolayer: tau2m = %e J = %f kBT'
-        % ( tau2m, tau2m * J__in__kBTroom ) )
+        % ( tau2mNeg, tau2mNeg * J__in__kBTroom ) )
 
 # sort and dump 'negative' profiles:
-for i in range( 0, nSlabsHalf ):
+for i in range( 0, nSlabs/2 ):
   outFile1.write( '%f %f\n' % (coord[i],-tau1m_z[i]) )
   outFile2.write( '%f %f\n' % (coord[i], tau2m_z[i]) )
 
 # calc 1st integral moment of lpp across 'positive' monolayer:
-tau1m = 0 # initialize 1st mom integral sum
-tau2m = 0 # initialize 2nd mom integral sum
-for i in range( nSlabsHalf, nSlabs ): # i = nSlabsHalf, ..., nSlabs-1
-  tau1m += coord[i] * lp[i] * delta
-  tau2m += coord[i]**2 * lp[i] * delta
-  outFile1.write( '%f %f\n' % (coord[i],tau1m) )
-  outFile2.write( '%f %f\n' % (coord[i],tau2m) )
-tau1m = tau1m * atmA2__in__J_nm
-tau2m = tau2m * atmA3__in__J
+tau1mPos = 0 # initialize 1st mom integral sum
+tau2mPos = 0 # initialize 2nd mom integral sum
+for i in range( nSlabs/2, nSlabs ): # i = nSlabs/2, ..., nSlabs-1
+  tau1mPos += coord[i] * lp[i] * delta
+  tau2mPos += coord[i]**2 * lp[i] * delta
+  outFile1.write( '%f %f\n' % (coord[i],tau1mPos) )
+  outFile2.write( '%f %f\n' % (coord[i],tau2mPos) )
+tau1mPos = tau1mPos * atmA2__in__J_nm
+tau2mPos = tau2mPos * atmA3__in__J
 print ( '1st int mom for \'+\' monolayer: tau1m = %e J/nm = %f kBT/nm'
-        % ( tau1m, tau1m * J__in__kBTroom ) )
+        % ( tau1mPos, tau1mPos * J__in__kBTroom ) )
 print ( '2nd int mom for \'+\' monolayer: tau2m = %e J = %f kBT'
-        % ( tau2m, tau2m * J__in__kBTroom ) )
+        % ( tau2mPos, tau2mPos * J__in__kBTroom ) )
 
-
+# calc & print averages:
+tau1mAvg = 0.5 * ( tau1mNeg + tau1mPos )
+print ( 'tau1m averaged over the two monolayers = %e J/nm = %f kBT/nm' 
+        % ( tau1mAvg, tau1mAvg * J__in__kBTroom ) )
+tau2mAvg = 0.5 * ( tau2mNeg + tau2mPos )
+print ( 'tau2m averaged over the two monolayers = %e J = %f kBT'
+        % ( tau2mAvg, tau2mAvg * J__in__kBTroom ) )
